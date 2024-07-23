a7d98335da1bc2a0f0983f1112d66e212e43eaeb
FH2007 <ohotni4ek90@mail.ru>
2024-07-23 18:40:25 +0500

    Автоматическое создание таблицы и процедуры при запуске

--- UploadDB/ApplicationContext.cs
+++ UploadDB/ApplicationContext.cs
@@ -7,6 +7,25 @@ namespace UploadDB
         public ApplicationContext()
         {
             Database.EnsureCreated();
+            EnsureTableCreated();
+            EnsureStoredProcedureCreated();
+        }
+
+        private void EnsureStoredProcedureCreated()
+        {
+            Database.ExecuteSqlRaw("CREATE or ALTER PROCEDURE InsertWords (@word varchar(20), @Count integer)\r\n" +
+                "as\r\n" +
+                "BEGIN\r\n    " +
+                "IF EXISTS (SELECT id FROM dbo.Words WHERE Word=@word ) \t\t\r\n\t\t" +
+                "UPDATE dbo.Words SET Count = Count + @Count where Word=@word;\t\t\r\n\telse\r\n\t\t" +
+                "INSERT INTO dbo.Words (Word, Count) VALUES(@word, @Count);\r\n" +
+                "END;");
+        }
+
+        private void EnsureTableCreated()
+        {
+            Database.ExecuteSqlRaw("IF OBJECT_ID(N'dbo.Words', N'U') IS NULL\r\n" +
+                "CREATE TABLE Words ( id INTEGER NOT NULL IDENTITY(1,1) PRIMARY KEY, word VARCHAR(20), Count integer )");
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- UploadDB/Scripts/CreateTableAndProcedure.sql
+++ UploadDB/Scripts/CreateTableAndProcedure.sql
@@ -1,6 +1,7 @@
+IF OBJECT_ID(N'dbo.Words', N'U') IS NULL
 CREATE TABLE Words ( id INTEGER NOT NULL IDENTITY(1,1) PRIMARY KEY, word VARCHAR(20), Count integer )
 go
-CREATE PROCEDURE InsertWords (@word varchar(20), @Count integer)
+CREATE or ALTER PROCEDURE InsertWords (@word varchar(20), @Count integer)
 as
 BEGIN
     IF EXISTS (SELECT id FROM dbo.Words WHERE Word=@word ) 		
